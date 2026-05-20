e4f04268cd82b951cf1c1e31690507656ced6dd7
Julien Habibi <julien.hi@yahoo.com>
2026-05-20 17:14:18 +0200

    feat: implement responsive article  hero section

--- src/components/article/HeroArticleSection/HeroArticleSection.tsx
+++ src/components/article/HeroArticleSection/HeroArticleSection.tsx
@@ -1,5 +1,5 @@
 import type { HeroArticleContentBlock } from "@/types";
-
+import styles from "./HeroArticleSection.module.scss";
 type HeroArticleSectionProps = HeroArticleContentBlock;
 
 export function HeroArticleSection({
@@ -11,17 +11,21 @@ export function HeroArticleSection({
   sideAsset,
 }: HeroArticleSectionProps) {
   return (
-    <section>
-      <img src={backgroundAsset.url} alt="" aria-hidden="true" />
-      <img src={sideAsset.url} alt="" aria-hidden="true" />
-
-      <div>
-        <h1>{title}</h1>
-        <p>{subtitle}</p>
-        <p>
-          {author} · {publishingDate}
-        </p>
+    <section className={styles.hero}>
+      <div className={styles.media}>
+        <img className={styles.leftImage} src={sideAsset.url} alt="" aria-hidden="true" />
+        <div className={styles.rightPanel}>
+        <img className={styles.rightImage} src={backgroundAsset.url} alt="" aria-hidden="true" />
+          <div className={styles.content}>
+          <p>{subtitle}</p>
+          <h1>{title}</h1>
+          </div>
+        </div>
       </div>
-    </section>
+ 
+      <p className={styles.meta}>
+        <span>{author}</span> · <span>{publishingDate}</span>
+      </p>
+  </section>
   );
 }
